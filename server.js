fe22e4b36ff071eac6f7663c87401856a5a86e5e
Farid-SolaradAi <farid.ahmed@solarad.ai>
2023-07-12 12:13:56 +0530

    add hashed api authentication

--- server.js
+++ server.js
@@ -3,34 +3,38 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const fileSystem = require('fs');
+const bcrypt = require('bcrypt');
 
 app.get('/health', (req, res) => {
     res.send(req.query.filename);
 });
 
 app.get('/getfilefroms3', async (req, res) => {
-    let api_key = req.query.api_key;
-    if (api_key === process.env.API_KEY) {
-        let site_id = req.query.site_id;
-        var filePath = `/home/ec2-user/s3-solaradoutput/Fenice/site_${site_id}.csv`;
-        var stat = fileSystem.statSync(filePath);
+    let providedApiKey = req.query.api_key;
+    const storedApiKey = process.env.API_KEY;
 
-        res.writeHead(200, {
-            'Content-Type': 'text/csv',
-            'Content-Length': stat.size
-        });
+    bcrypt.compare(providedApiKey, storedApiKey, (err, result) => {
+        if (result) {
+            let site_id = req.query.site_id;
+            var filePath = `/home/ec2-user/s3-solaradoutput/Fenice/site_${site_id}.csv`;
+            var stat = fileSystem.statSync(filePath);
 
-        var readStream = fileSystem.createReadStream(filePath);
-        readStream.pipe(res);
-    }
-    else {
-        res.send("User Unauthorized");
-    }
+            res.set('Content-Disposition', `attachment; filename=site_${site_id}.csv`);
+            res.set('Content-Type', 'text/csv');
+            res.set('Content-Length', stat.size);
+
+            var readStream = fileSystem.createReadStream(filePath);
+            readStream.pipe(res);
+        } else {
+            res.status(401).send("Unauthorized");
+        }
+    });
 });
+
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || `localhost`;
 
 app.listen(3000, () => {
     console.log(`Server is running at http://${HOST}:${PORT}`);
-})
+});
