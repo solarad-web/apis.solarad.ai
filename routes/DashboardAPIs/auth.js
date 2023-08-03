944880b3b9741eb39cd3946b273d344b612ef396
Farid-SolaradAi <farid.ahmed@solarad.ai>
2023-08-03 18:26:29 +0530

    increase validity time for email verify mail to 24hours

--- routes/DashboardAPIs/auth.js
+++ routes/DashboardAPIs/auth.js
@@ -26,7 +26,7 @@ route.get("/signUp", async (req, res, next) => {
         }
         console.log(process.env.JWT_SECRET);
         const token = jwt.sign({ email: email, fname: fname, lname: lname, pwd: pwd, company: company }, process.env.JWT_SECRET, {
-            expiresIn: "1h",
+            expiresIn: "24h",
         })
         await sendMagicLinkEmail({ email: email, token: token, fname: fname });
         res.status(200).send('Email Sent');
