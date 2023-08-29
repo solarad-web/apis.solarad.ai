705c2e1820171f5183e3373ddd165a1c2dcc0760
Farid-SolaradAi <farid.ahmed@solarad.ai>
2023-08-29 17:22:26 +0530

    reconnect old db

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: process.env.DBUSER,
-    password: process.env.DBPASSWORD,
-    host: process.env.DBHOST,
-    port: process.env.DBPORT,
-    database: process.env.DBNAME,
-    ssl: {
-        rejectUnauthorized: false,
-    }
+    user: "user",
+    password: "pass",
+    host: 'postgres',
+    port: 5432,
+    database: "db"
 });
 
 module.exports = pool;
+
+
+
+
