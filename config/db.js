88a550a51712e5163c4b90e2778056e8fb7208a0
Farid-SolaradAi <farid.ahmed@solarad.ai>
2023-08-29 17:04:03 +0530

    reconnect rds db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "user",
-    password: "pass",
-    host: 'postgres',
-    port: 5432,
-    database: "db"
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
+    database: process.env.DBNAME,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 });
 
 module.exports = pool;
