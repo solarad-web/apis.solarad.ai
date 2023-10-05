20b781ac3d6bf7d405acdc7be102e4ae5c95757c
Farid-SolaradAi <farid.ahmed@solarad.ai>
2023-10-05 23:54:33 +0530

    test

--- routes/DashboardAPIs/sites.js
+++ routes/DashboardAPIs/sites.js
@@ -564,15 +564,13 @@ route.get('/getforecastFromDb', async (req, res, next) => {
             mergedData.push(rowToMerge);
         }
 
-        res.send(mergedData)
+        const json2csvParser = new Parser();
+        const csvData = json2csvParser.parse(mergedData);
 
-        // const json2csvParser = new Parser();
-        // const csvData = json2csvParser.parse(mergedData);
-
-        // // Set response headers for CSV and send the data
-        // res.setHeader('Content-Type', 'text/csv');
-        // res.setHeader('Content-Disposition', 'attachment; filename=forecast.csv');
-        // res.send(csvData);
+        // Set response headers for CSV and send the data
+        res.setHeader('Content-Type', 'text/csv');
+        res.setHeader('Content-Disposition', 'attachment; filename=forecast.csv');
+        res.send(csvData);
 
 
 
