4d79b980996936b5628c27f889f27e88224a112d
Farid-SolaradAi <farid.ahmed@solarad.ai>
2023-11-06 13:42:56 +0530

    remove version from run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,6 @@
-docker build --no-cache -t apis.solarad.ai:v1 .
+docker build --no-cache -t apis.solarad.ai .
 
 docker run --rm -p 80:3000 \
   --pid=host \
   -v /home/ec2-user/efs-solarad-output:/home/ \
-  -it apis.solarad.ai:v1
+  -it apis.solarad.ai
